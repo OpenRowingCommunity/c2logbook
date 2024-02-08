946df08bf32932af7592928ee5ac5cbd3a9cbc80
Adrian Edwards <adrian@adriancedwards.com>
2024-02-08 13:32:20 -0500

    dart create library project

---
+++ example/c2logbook_example.dart
@@ -0,0 +1,6 @@
+import 'package:c2logbook/c2logbook.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/c2logbook_test.dart
@@ -0,0 +1,16 @@
+import 'package:c2logbook/c2logbook.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
