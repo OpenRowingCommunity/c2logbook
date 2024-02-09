be0bb0622d4dd36211fe411446d1a7b23587d461
Adrian Edwards <adrian@adriancedwards.com>
2024-02-09 15:40:10 -0500

    add custom oauth client class for c2

---
+++ lib/src/c2_oauth_client.dart
@@ -0,0 +1,15 @@
+import 'package:oauth2_client/oauth2_client.dart';
+
+class Concept2OAuth2Client extends OAuth2Client {
+  String baseUrl;
+
+  Concept2OAuth2Client(
+      {required this.baseUrl,
+      required String redirectUri,
+      required String customUriScheme})
+      : super(
+            authorizeUrl: '$baseUrl/oauth/authorize',
+            tokenUrl: '$baseUrl/oauth/access_token',
+            redirectUri: redirectUri,
+            customUriScheme: customUriScheme);
+}
