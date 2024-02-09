779bd902214ce3db73b57422bfeb61787a4dab12
Adrian Edwards <adrian@adriancedwards.com>
2024-02-08 19:31:01 -0500

    rename server url getter

--- lib/src/c2logbook_base.dart
+++ lib/src/c2logbook_base.dart
@@ -1,8 +1,9 @@
 class C2Logbook {
   bool development;
 
-  String get _server_url =>
-      development ? 'https://log-dev.concept2.com' : 'https://log.concept2.com';
+  Uri get _server_uri => Uri.https(development
+      ? 'https://log-dev.concept2.com'
+      : 'https://log.concept2.com');
 
   C2Logbook({this.development = false});
 
