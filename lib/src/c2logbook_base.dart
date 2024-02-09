d3d4e295cd068fe0aa3adac01ebc53e88d65b2bb
Adrian Edwards <adrian@adriancedwards.com>
2024-02-08 19:30:50 -0500

    add initializer

--- lib/src/c2logbook_base.dart
+++ lib/src/c2logbook_base.dart
@@ -1,8 +1,11 @@
 class C2Logbook {
-  bool development = false;
+  bool development;
 
   String get _server_url =>
       development ? 'https://log-dev.concept2.com' : 'https://log.concept2.com';
+
+  C2Logbook({this.development = false});
+
 
       
 }
