05e8621caf05f7fe1c6f8972107316bcadfd13df
Adrian Edwards <adrian@adriancedwards.com>
2024-02-09 16:09:41 -0500

    remove extra https from server uri value

--- lib/src/c2logbook_base.dart
+++ lib/src/c2logbook_base.dart
@@ -3,9 +3,8 @@ import 'package:oauth2_client/oauth2_helper.dart';
 class C2Logbook {
   bool development;
 
-  Uri get _serverUri => Uri.https(development
-      ? 'https://log-dev.concept2.com'
-      : 'https://log.concept2.com');
+  Uri get _serverUri =>
+      Uri.https(development ? 'log-dev.concept2.com' : 'log.concept2.com');
 
   Map<String, String>? get _headers =>
       <String, String>{'Accept': 'application/vnd.c2logbook.v1+json'};
