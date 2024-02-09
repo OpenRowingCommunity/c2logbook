6c89b5d777086f13b4ec71d4f1505597499215fb
Adrian Edwards <adrian@adriancedwards.com>
2024-02-09 15:38:52 -0500

    rename server uri variable

--- lib/src/c2logbook_base.dart
+++ lib/src/c2logbook_base.dart
@@ -1,7 +1,7 @@
 class C2Logbook {
   bool development;
 
-  Uri get _server_uri => Uri.https(development
+  Uri get _serverUri => Uri.https(development
       ? 'https://log-dev.concept2.com'
       : 'https://log.concept2.com');
 
