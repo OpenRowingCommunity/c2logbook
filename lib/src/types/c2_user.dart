0cc0f5d1780372b428445467d8b0a50864d5b2f6
Adrian Edwards <adrian@adriancedwards.com>
2024-02-09 21:39:25 -0500

    format identifiers to the way that Dart likes it

--- lib/src/types/c2_user.dart
+++ lib/src/types/c2_user.dart
@@ -1,41 +1,53 @@
 class C2User {
   int id;
   String username;
-  String first_name;
-  String last_name;
+  String firstName;
+  String lastName;
   String gender;
   String dob;
   String email;
   String country;
-  String? profile_image;
-  bool age_restricted;
-  bool email_permission;
-  int? max_heart_rate;
+  String? profileImage;
+  bool ageRestricted;
+  bool emailPermission;
+  int? maxHeartRate;
   int? weight;
   List<String> roles;
-  String logbook_privacy;
+  String logbookPrivacy;
 
-  C2User({this.id = 0, this.username = '', this.first_name = '', this.last_name = '', this.gender = 'F', this.dob = '1970-01-01', this.email = '', this.country = '', this.profile_image, this.age_restricted = false, this.email_permission = false, this.max_heart_rate, this.weight, this.roles = const [], this.logbook_privacy = ''});
+  C2User(
+      {this.id = 0,
+      this.username = '',
+      this.firstName = '',
+      this.lastName = '',
+      this.gender = 'F',
+      this.dob = '1970-01-01',
+      this.email = '',
+      this.country = '',
+      this.profileImage,
+      this.ageRestricted = false,
+      this.emailPermission = false,
+      this.maxHeartRate,
+      this.weight,
+      this.roles = const [],
+      this.logbookPrivacy = ''});
 
   factory C2User.fromJson(Map<String, dynamic> json) {
     return C2User(
-      id: json['id'],
-      username: json['username'],
-      first_name: json['first_name'],
-      last_name: json['last_name'],
-      gender: json['gender'],
-      dob: json['dob'],
-      email: json['email'],
-      country: json['country'],
-      profile_image: json['profile_image'],
-      age_restricted: json['age_restricted'],
-      email_permission: json['email_permission'],
-      max_heart_rate: json['max_heart_rate'],
-      weight: json['weight'],
-      roles: json['roles'],
-      logbook_privacy: json['logbook_privacy']
-    );
+        id: json['id'],
+        username: json['username'],
+        firstName: json['first_name'],
+        lastName: json['last_name'],
+        gender: json['gender'],
+        dob: json['dob'],
+        email: json['email'],
+        country: json['country'],
+        profileImage: json['profile_image'],
+        ageRestricted: json['age_restricted'],
+        emailPermission: json['email_permission'],
+        maxHeartRate: json['max_heart_rate'],
+        weight: json['weight'],
+        roles: json['roles'],
+        logbookPrivacy: json['logbook_privacy']);
   }
-
-
 }
