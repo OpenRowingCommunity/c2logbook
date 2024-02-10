01f35071c1fc7f7a8b25df9e6510f79579af7042
Adrian Edwards <adrian@adriancedwards.com>
2024-02-09 22:03:08 -0500

    refactor UI a bit to display user info

--- example/lib/src/userinfo.dart
+++ example/lib/src/userinfo.dart
@@ -1,20 +1,36 @@
 import 'package:c2logbook/c2logbook.dart';
 import 'package:flutter/material.dart';
-import 'package:pa1_source/src/userinfo.dart';
 import 'package:provider/provider.dart';
 
-class LogbookView extends StatelessWidget {
-  const LogbookView({Key? key}) : super(key: key);
+class UserInfo extends StatelessWidget {
+  final C2User me;
+  const UserInfo({Key? key, required this.me}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Hello ${me.firstName} ${me.lastName}"),
+          Text("Your Country: ${me.country}"),
+          Text("Your Gender: ${me.gender}"),
+          Text("Your Birthday: ${me.dob}"),
+        ],
+      ),
+    );
     return FutureBuilder<C2User>(
         future: Provider.of<C2Logbook>(context, listen: false)
             .me(), // a previously-obtained Future<String> or null
         builder: (BuildContext context, AsyncSnapshot<C2User> snapshot) {
           List<Widget> children;
           if (snapshot.hasData) {
-            children = <Widget>[UserInfo(me: snapshot.data!)];
+            children = <Widget>[
+              Text("Hello ${me.firstName} ${me.lastName}"),
+              Text("Your Country: ${me.country}"),
+              Text("Your Gender: ${me.gender}"),
+              Text("Your Birthday: ${me.dob}"),
+            ];
           } else if (snapshot.hasError) {
             children = <Widget>[
               const Icon(
