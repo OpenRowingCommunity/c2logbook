5a034bb7f1d2f283fd75f1258723fcd7c126e882
Adrian Edwards <adrian@adriancedwards.com>
2024-02-09 21:48:19 -0500

    add a function to fetch the users own profile

--- lib/src/c2logbook_base.dart
+++ lib/src/c2logbook_base.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:c2logbook/src/c2_oauth_client.dart';
+import 'package:c2logbook/src/types/c2_user.dart';
 import 'package:oauth2_client/oauth2_helper.dart';
 import 'package:http/http.dart' as http;
 class C2Logbook {
@@ -55,5 +58,13 @@ class C2Logbook {
   Future<http.Response> _delete(String url,
       {Map<String, String>? headers, http.Client? httpClient}) async {
     return oauthHelper.delete(url, headers: headers, httpClient: httpClient);
+  }
+
+  Future<C2User> me() async {
+    return _get(_serverUri.resolve("/api/users/me").toString())
+        .then((response) => json.decode(response.body))
+        .then(
+          (json) => C2User.fromJson(json['data']),
+        );
   }
 }
