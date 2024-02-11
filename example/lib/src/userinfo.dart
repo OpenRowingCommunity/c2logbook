4c96bbd09614f4a8d7ff856899e85e0f75830881
Adrian Edwards <adrian@adriancedwards.com>
2024-02-11 11:44:24 -0500

    remove dead code from userinfo

--- example/lib/src/userinfo.dart
+++ example/lib/src/userinfo.dart
@@ -19,39 +19,5 @@ class UserInfo extends StatelessWidget {
         ],
       ),
     );
-    return FutureBuilder<C2User>(
-        future: Provider.of<C2Logbook>(context, listen: false)
-            .me(), // a previously-obtained Future<String> or null
-        builder: (BuildContext context, AsyncSnapshot<C2User> snapshot) {
-          List<Widget> children;
-          if (snapshot.hasData) {
-            children = <Widget>[
-              Text("Hello ${me.firstName} ${me.lastName}"),
-              Text("Your Country: ${me.country}"),
-              Text("Your Gender: ${me.gender}"),
-              Text("Your Birthday: ${me.dob}"),
-            ];
-          } else if (snapshot.hasError) {
-            children = <Widget>[
-              const Icon(
-                Icons.error_outline,
-                color: Colors.red,
-                size: 60,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16),
-                child: Text('Error: ${snapshot.error}'),
-              ),
-            ];
-          } else {
-            children = <Widget>[Text("Unknown")];
-          }
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: children,
-            ),
-          );
-        });
   }
 }
