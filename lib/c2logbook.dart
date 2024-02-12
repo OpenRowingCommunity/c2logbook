33681434fb097867549d6b3aef839dfe41236093
Adrian Edwards <adrian@adriancedwards.com>
2024-02-12 16:04:57 -0500

    publish c2results types

--- lib/c2logbook.dart
+++ lib/c2logbook.dart
@@ -5,5 +5,7 @@ library;
 
 export 'src/c2logbook_base.dart';
 export 'src/types/c2_user.dart';
+export 'src/types/c2_results.dart';
+
 
 // TODO: Export any libraries intended for clients of this package.
