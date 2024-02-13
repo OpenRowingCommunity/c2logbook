9db0d3cc1fea555e38ba9c2354bd879c71c93ce8
Adrian Edwards <adrian@adriancedwards.com>
2024-02-12 20:53:48 -0500

    use e.name for getting enum value from string

--- lib/src/types/c2_types.dart
+++ lib/src/types/c2_types.dart
@@ -1,10 +1,10 @@
 // TODO: add conversion to string to allow logged_in to be represented in dart as loggedIn
 enum C2PrivacyLevel { private, partners, logged_in, everyone }
 
+//https://stackoverflow.com/a/44060511/
 extension C2PrivacyExtension on C2PrivacyLevel {
-  static C2PrivacyLevel fromString(String value) {
-    return C2PrivacyLevel.values
-        .firstWhere((e) => e.toString() == 'C2PrivacyLevel.$value');
+  static C2PrivacyLevel? fromString(String value) {
+    return C2PrivacyLevel.values.firstWhere((e) => e.name == value);
   }
 }
 
