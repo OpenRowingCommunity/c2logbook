be8938d5aede592dd98cbb94145ea3aa57e6cad3
Adrian Edwards <adrian@adriancedwards.com>
2024-03-06 15:21:50 -0500

    add TimestampOrNull Converter

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -12,3 +12,20 @@ class TimestampConverter implements JsonConverter<DateTime, String> {
   @override
   String toJson(DateTime date) => date.toIso8601String();
 }
+
+//https://stackoverflow.com/a/69749532
+class TimestampOrNullConverter implements JsonConverter<DateTime?, String?> {
+  const TimestampOrNullConverter();
+
+  @override
+  DateTime? fromJson(String? timestamp) {
+    if (timestamp == null) {
+      return null;
+    }
+    return DateTime.parse(timestamp);
+  }
+
+  @override
+  String? toJson(DateTime? date) =>
+      date == null ? null : date.toIso8601String();
+}
