b46deaba7ba3968da3c6cbcb7e2a155e5a9d8e53
Adrian Edwards <adrian@adriancedwards.com>
2024-03-06 15:49:03 -0500

    make date field required since there is no default

--- lib/src/types/c2_results.dart
+++ lib/src/types/c2_results.dart
@@ -18,7 +18,7 @@ class C2Results with _$C2Results {
   factory C2Results({
     @Default(0) int id,
     @JsonKey(name: "user_id") @Default(0) int userId,
-    @JsonKey(name: "date") @TimestampConverter() DateTime date,
+    @JsonKey(name: "date") @TimestampConverter() required DateTime date,
     @JsonKey(name: "date_utc") @TimestampOrNullConverter() DateTime? dateUtc,
     String? timezone,
     @Default(0) int distance,
