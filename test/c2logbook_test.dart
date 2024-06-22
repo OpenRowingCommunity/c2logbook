6775961b5eaae6649ab1b34334ee43653b87155e
Adrian Edwards <adrian@adriancedwards.com>
2024-06-22 13:48:45 -0400

    add a real data sample to the unit tests that contains a workout object

--- test/c2logbook_test.dart
+++ test/c2logbook_test.dart
@@ -181,6 +181,121 @@ void main() {
     }""");
       expect(C2Webhook.parse(webhookJson), testResultsMin);
     });
+
+    test("Test parsing Result JSON (with intervals workout and Heart rate)",
+        () {
+      // anonymized sample data provided from a real-world usecase
+      final dynamic webhookJson = json.decode("""{
+    "id": 87654321,
+    "user_id": 1234567,
+    "date": "2024-04-22 17:41:00",
+    "timezone": "America/New_York",
+    "date_utc": "2024-04-22 21:41:00",
+    "distance": 11193,
+    "type": "rower",
+    "time": 27000,
+    "time_formatted": "45:00.0",
+    "workout_type": "FixedTimeSplits",
+    "source": "ErgData Android",
+    "weight_class": "H",
+    "verified": true,
+    "ranked": false,
+    "comments": null,
+    "privacy": "private",
+    "stroke_data": true,
+    "calories_total": 740,
+    "drag_factor": 121,
+    "stroke_count": 1023,
+    "stroke_rate": 22,
+    "heart_rate": {
+    "average": 145,
+    "min": 0,
+    "max": 0,
+    "ending": 145,
+    "recovery": 0
+    },
+    "workout": {
+    "splits": [
+        {
+        "time": 5400,
+        "distance": 2244,
+        "calories_total": 151,
+        "stroke_rate": 22,
+        "heart_rate": {
+            "average": 139,
+            "min": 136,
+            "max": 146,
+            "ending": 142,
+            "recovery": 0,
+            "rest": 0
+        }
+        },
+        {
+        "time": 5400,
+        "distance": 2270,
+        "calories_total": 152,
+        "stroke_rate": 23,
+        "heart_rate": {
+            "average": 144,
+            "min": 140,
+            "max": 147,
+            "ending": 147,
+            "recovery": 0,
+            "rest": 0
+        }
+        }
+    ],
+    "targets": {
+        "heart_rate_zone": 0
+    }
+    },
+    "real_time": null
+    }""");
+      expect(
+          C2Results.fromJson(webhookJson),
+          C2Results(
+              id: 87654321,
+              userId: 1234567,
+              endDate: DateTime.parse("2024-04-22 17:41:00"),
+              dateUtc: DateTime.parse("2024-04-22 21:41:00"),
+              timezone: "America/New_York",
+              distance: 11193,
+              type: C2ResultType.rower,
+              time: 2700.0,
+              strokeRate: 22,
+              workoutType: C2APIWorkoutType.FixedTimeSplits,
+              source: "ErgData Android",
+              weightClass: C2WeightClass.heavyweight,
+              verified: true,
+              ranked: false,
+              comments: null,
+              privacy: C2PrivacyLevel.private,
+              // restDistance: 887,
+              // restTime: 900.0,
+              // stroke_data True
+              caloriesTotal: 740,
+              dragFactor: 121,
+              strokeCount: 1023,
+              heartRate: C2HeartRate(min: 0, average: 145, max: 0, ending: 145),
+              workout: C2Workout(splits: <C2Splits>[
+                C2Splits(
+                  time: 540.0,
+                  distance: 2244,
+                  caloriesTotal: 151,
+                  strokeRate: 22,
+                  heartRate: C2HeartRate(
+                      min: 136, average: 139, max: 146, ending: 142),
+                ),
+                C2Splits(
+                  time: 540.0,
+                  distance: 2270,
+                  caloriesTotal: 152,
+                  strokeRate: 23,
+                  heartRate: C2HeartRate(
+                      min: 140, average: 144, max: 147, ending: 147),
+                )
+              ])));
+    });
   });
 
   group('Equality Tests', () {
