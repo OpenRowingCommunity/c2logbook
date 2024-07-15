5ee7fbf7dfc8de5086ec7a81c1e4476eebbfac3b
Adrian Edwards <adrian@adriancedwards.com>
2024-07-14 20:24:47 -0400

    document some of the main type classes used by the library

--- lib/src/types/c2_types.dart
+++ lib/src/types/c2_types.dart
@@ -1,8 +1,11 @@
 // TODO: add conversion to string to allow logged_in to be represented in dart as loggedIn
 part of 'index.dart';
 
+/// Represents the a level of privacy for a particular record in the Concept2 REST API
+/// This determines who can see data being uploaded, such as workouts.
 enum C2PrivacyLevel { private, partners, logged_in, everyone }
 
+/// Represents the type of a particluar result from the Concept2 REST API
 enum C2ResultType {
   rower,
   skierg,
@@ -17,6 +20,7 @@ enum C2ResultType {
 }
 
 // TODO: deduplicate this with C2Bluetooth
+/// Represents the type of a particluar workout from the Concept2 REST API
 enum C2APIWorkoutType {
   unknown,
   JustRow,
@@ -30,6 +34,7 @@ enum C2APIWorkoutType {
   VariableIntervalUndefinedRest,
 }
 
+/// Represents the weight class of the user for a particluar result in the Concept2 REST API
 enum C2WeightClass {
   @JsonValue("L")
   lightweight,
