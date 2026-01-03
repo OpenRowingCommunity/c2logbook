532a88d8e748644d855e2a121a6261d360c86789
Adrian Edwards <adrian@adriancedwards.com>
2026-01-02 16:24:36 -0800

    Add c2workout type Co-Authored-By: pfredCL <95325187+pfredCL@users.noreply.github.com>

--- lib/src/types/index.dart
+++ lib/src/types/index.dart
@@ -10,5 +10,6 @@ part 'c2_type_extensions.dart';
 part 'c2_types.dart';
 part 'c2_user.dart';
 part 'c2_webhook_result.dart';
+part 'c2_workout.dart';
 part 'index.freezed.dart';
 part 'index.g.dart';
